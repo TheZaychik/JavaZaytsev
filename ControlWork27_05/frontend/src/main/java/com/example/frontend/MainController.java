67b88e12afa9361546c3e34bd86790b1dce7ede9
Nikita Zaystev <nikitos945max@gmail.com>
2023-05-27 14:29:18 +0300

    upd

---
+++ ControlWork27_05/frontend/src/main/java/com/example/frontend/MainController.java
@@ -0,0 +1,19 @@
+package com.example.frontend;
+
+import com.example.frontend.schemas.Film;
+import javafx.fxml.FXML;
+import javafx.scene.control.TableView;
+
+public class MainController {
+    @FXML
+    private TableView<Film> mainTable;
+
+    @FXML
+    protected void initialize(){
+
+    }
+    @FXML
+    protected void onHelloButtonClick() {
+
+    }
+}
---
+++ ControlWork27_05/frontend/src/main/java/com/example/frontend/schemas/Film.java
@@ -0,0 +1,10 @@
+package com.example.frontend.schemas;
+
+public class Film {
+    public Integer id;
+    public String name;
+    public String genre;
+    public Integer duration;
+    public Integer year;
+
+}
